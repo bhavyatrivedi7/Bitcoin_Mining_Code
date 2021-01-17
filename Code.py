05d8e7071656a68bfa68498f111304e81ce3729e
Bhavya Trivedi <60940701+bhavyatrivedi7@users.noreply.github.com>
2021-01-17 15:56:45 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,23 @@
+from hashlib import sha256
+MAX_NONCE = 100000000000
+
+def SHA256(text):
+    return sha256(text.encode("ascii")).hexdigest()
+
+def mine(block_number, transactions, previous_hash, prefix_zeros):
+    prefix_str = '0'*prefix_zeros
+    for nonce in range(MAX_NONCE):
+        text = str(block_number) + transactions + previous_hash + str(nonce)
+        new_hash = SHA256(text)
+        if new_hash.startswith(prefix_str):
+            print(f"Yay! Successfully mined bitcoins with nonce value:{nonce}")
+            return new_hash
+
+    raise BaseException(f"Couldn't find correct has after trying {MAX_NONCE} times")
+
+if __name__=='__main__':
+    transactions=""
+    difficulty=8
+    print("start mining")
+    new_hash = mine(5,transactions,'0000000xa036944e29568d0cff17edbe038f81208fecf9a66be9a2b8321c6ec7', difficulty)
+    print(new_hash)
